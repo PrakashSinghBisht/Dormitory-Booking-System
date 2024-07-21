532b0c13c35c22abb86c492340f144b0e2cc0843
PrakashSinghBisht <prakash0005singh@gmail.com>
2024-07-22 02:50:33 +0530

    DB related changes

--- Dormitory Reservation System/DormitoryReservationSystem.java
+++ Dormitory Reservation System/DormitoryReservationSystem.java
@@ -8,7 +8,7 @@ import java.sql.ResultSet;
 
 
 public class DormitoryReservationSystem {
-    private static final String url = "jdbc:mysql://localhost:3306/hotel_db";
+    private static final String url = "jdbc:mysql://localhost:3306/dorm_db";
     private static final String username = "root";
     private static final String password = "Prakash@1";
 
